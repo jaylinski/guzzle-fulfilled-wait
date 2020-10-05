3524956e9aafda088504b5e5fab9a419931478cf
Jakob Linskeseder <jakob.linskeseder@karriere.at>
2020-10-05 13:55:58 +0200

    Improve

--- test.php
+++ test.php
@@ -4,15 +4,27 @@ require __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Promise\FulfilledPromise;
 use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Promise\PromiseInterface;
 
 $data = [1, 2, 3];
 
+function getPromise(): PromiseInterface {
+    global $data;
+    $promise = new Promise(function () use (&$promise, $data) {
+        echo '1 $promise resolving wait fn' . PHP_EOL;
+        $promise->resolve($data);
+    });
+    return $promise;
+}
+
+function getFulfilledPromise(): PromiseInterface {
+    global $data;
+    return new FulfilledPromise($data);
+}
+
 // Normal promise
 echo 'Testing normal promise:' . PHP_EOL . PHP_EOL;
-$promise = new Promise(function () use (&$promise, $data) {
-    echo '1 $promise resolving wait fn' . PHP_EOL;
-    $promise->resolve($data);
-});
+$promise = getPromise();
 $promise->then(function () use ($data) {
     echo '2 $promise->then() called' . PHP_EOL;
 });
@@ -23,7 +35,7 @@ echo '4 AFTER $promise->wait()' . PHP_EOL . PHP_EOL;
 
 // Fulfilled promise
 echo 'Testing fulfilled promise:' . PHP_EOL . PHP_EOL;
-$promiseFulFilled = new FulfilledPromise($data);
+$promiseFulFilled = getFulfilledPromise();
 $promiseFulFilled->then(function () use ($data) {
     echo '1 $promise resolving wait fn' . PHP_EOL; // <- will only get called if we run the global queue promise
 });
@@ -34,7 +46,7 @@ echo '3 AFTER $promiseFulFilled->wait()' . PHP_EOL . PHP_EOL;
 
 // Fulfilled promise (working as expected)
 echo 'Testing fulfilled promise (fixed):' . PHP_EOL . PHP_EOL;
-$promiseFulFilledFixed = new FulfilledPromise($data);
+$promiseFulFilledFixed = getFulfilledPromise();
 $promiseFulFilledPromise = $promiseFulFilledFixed->then(function () use ($data) {
     echo '1 $promiseFulFilledFixed->then() called' . PHP_EOL;
 });
